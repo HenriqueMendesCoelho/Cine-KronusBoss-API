ac353a7c4f3f82c46b33161cde139f75de5c6338
KronusBoss <henriquemendescoelho@gmail.com>
2023-01-19 01:10:03 -0300

    fix(SpringControllerAdviceHandler): `MethodArgumentNotValidException handler` to work properly

--- src/main/java/com/kronusboss/cine/application/spring/handler/SpringControllerAdviceHandler.java
+++ src/main/java/com/kronusboss/cine/application/spring/handler/SpringControllerAdviceHandler.java
@@ -1,45 +1,40 @@
 package com.kronusboss.cine.application.spring.handler;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import com.kronusboss.cine.application.spring.handler.dto.HandlerErrorDto;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class SpringControllerAdviceHandler {
-	
+
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler({MethodArgumentNotValidException.class})
-	public Map<String, Object> handleValidationExceptions(
-			MethodArgumentNotValidException ex, HttpServletRequest request) {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, Object> handleValidationExceptions(MethodArgumentNotValidException ex,
+			HttpServletRequest request) {
 		Map<String, Object> response = new LinkedHashMap<>();
 		response.put("timestamp", new Date(System.currentTimeMillis()).toString());
 		response.put("status", 400);
 		response.put("error", "Bad Request");
-
-		List<HandlerErrorDto> errorsDTO = new ArrayList<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			HandlerErrorDto dto = HandlerErrorDto
-					.builder()
-					.key(((FieldError) error).getField())
-					.error(error.getDefaultMessage())
-					.build();
-			errorsDTO.add(dto);
-		});
-		response.put("message", errorsDTO);
 		response.put("path", request.getRequestURI());
+		
+		Map<String, Object> errors = new LinkedHashMap<>();
+	    ex.getBindingResult().getAllErrors().forEach((error) -> {
+	        String fieldName = ((FieldError) error).getField();
+	        String errorMessage = error.getDefaultMessage();
+	        errors.put(fieldName, errorMessage);
+	    });
+	    
+	    response.put("errors", errors);
+		
 		return response;
 	}
 }
