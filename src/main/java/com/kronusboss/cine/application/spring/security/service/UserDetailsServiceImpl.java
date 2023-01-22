2408a8f13b0274a67da7bb397151f848d8e20d18
KronusBoss <henriquemendescoelho@gmail.com>
2023-01-22 05:32:47 -0300

    feat: userDetailsSerice to jwt auth

---
+++ src/main/java/com/kronusboss/cine/application/spring/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.kronusboss.cine.application.spring.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.kronusboss.cine.adapter.repository.jpa.user.UserRepository;
+import com.kronusboss.cine.application.spring.security.UserSS;
+import com.kronusboss.cine.domain.user.User;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = repository.findByEmail(email);
+		
+		if(user == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(user.getId(), user.getName(), user.getEmail(), user.getPassword(), user.getRoles());
+	}
+
+}
