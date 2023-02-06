3b1c105b7e1b4bdc10279ef5796cea74d95e23af
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-02-05 22:01:04 -0300

    refactor(Movie): added column for id tmdb

--- src/main/java/com/kronusboss/cine/domain/movie/Movie.java
+++ src/main/java/com/kronusboss/cine/domain/movie/Movie.java
@@ -12,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -26,39 +25,36 @@ public class Movie {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column
 	private UUID id;
-	
-	@NotBlank
+
 	@Column(length=150, nullable = false)
 	private String portugueseTitle;
-	
-	@NotBlank
+
 	@Column(length=150, nullable = false)
-	private String englishTitle;
-	
-	@NotBlank
+	private String originalTitle;
+
 	@Column(length=150, nullable = false)
 	private String director;
-	
-	@NotBlank
+
 	@Column(columnDefinition = "text")
 	private String urlImage;
-	
-	@NotBlank
+
 	@Column(columnDefinition = "text")
 	private String portugueseUrlTrailer;
-	
-	@NotBlank
+
 	@Column(columnDefinition = "text")
 	private String englishUrlTrailer;
-	
-	@NotBlank
+
 	@Column(columnDefinition = "text")
 	private String description;
 	
 	@Column(columnDefinition = "numeric(4)")
 	private int year;
 	
+	@Column(nullable = true, unique = true)
+	private Long tmdbId;
+	
 	@JsonManagedReference
 	@OneToMany(mappedBy = "movie")
 	private List<MovieNote> notes;
+	
 }
