8d4a8b6bce19be667e2eee17d4518f37cc17c089
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-02-26 07:25:13 -0300

    refactor: login response json

--- src/main/java/com/kronusboss/cine/application/spring/security/JWTAuthenticationFilter.java
+++ src/main/java/com/kronusboss/cine/application/spring/security/JWTAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.kronusboss.cine.application.spring.security;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Set;
@@ -14,7 +15,9 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.kronusboss.cine.adapter.core.controller.dto.UserLoginDto;
 import com.kronusboss.cine.application.spring.security.util.JWTUtil;
 
@@ -22,6 +25,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.Setter;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -57,27 +62,52 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		}
 
 	}
-	
+
 	@Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-                                            Authentication auth) throws IOException, ServletException {
-		
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String name = ((UserSS) auth.getPrincipal()).getName();
 		Set<String> roles = ((UserSS) auth.getPrincipal()).getRoles();
-        String token = jwtUtil.generateToken(username, name, roles);
-        
-        long expires = new Date().getTime() + jwtUtil.expiration;
-        
-        response.setContentType("application/json");
-        
-        response.getWriter().append("{\"access_token\": \"" + token + "\", " +
-        		"\"expires\": \"" + new Date(expires) + "\"} ");
+		String token = jwtUtil.generateToken(username, name, roles);
+
+		response.setContentType("application/json");
+		response.getWriter().append(new LoginResponse(token).toJson());
 	}
 
 	private void logRequest(HttpServletRequest request, HttpServletResponse response) {
 		log.info("[" + response.getStatus() + "]" + "[" + request.getMethod() + "]" + request.getRequestURI() + "[from:"
 				+ request.getHeader("x-forwarded-for") + "]");
+	}
+
+	@Getter
+	@Setter
+	private class LoginResponse {
+
+		private String accessToken;
+		private Date expires;
+
+		public LoginResponse(String accessToken) {
+			this.accessToken = accessToken;
+			expires = new Date(new Date().getTime() + jwtUtil.expiration);
+		}
+
+		public String toJson() {
+			try {
+				ObjectMapper mapper = getObjectMapper();
+				return mapper.writeValueAsString(this);
+			} catch (JsonProcessingException e) {
+				log.error(e.getMessage());
+			}
+			return null;
+		}
+
+		private ObjectMapper getObjectMapper() {
+			ObjectMapper mapper = new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+			mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+			return mapper;
+		}
 	}
 
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
@@ -94,12 +124,9 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		}
 
 		private String jsonNormal() {
-            long date = new Date().getTime();
-            return "{\"timestamp\": " + date + ", "
-                + "\"status\": 401, "
-                + "\"error\": \"Not authorized\", "
-                + "\"message\": \"Email or password are incorrect.\", "
-                + "\"path\": \"/login\"}";
-        }
+			long date = new Date().getTime();
+			return "{\"timestamp\": " + date + ", " + "\"status\": 401, " + "\"error\": \"Not authorized\", "
+					+ "\"message\": \"Email or password are incorrect.\", " + "\"path\": \"/login\"}";
+		}
 	}
 }
