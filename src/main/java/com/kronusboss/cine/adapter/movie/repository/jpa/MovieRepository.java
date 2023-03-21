fa1c74960483eb7b8418384f0fda0dcd22b69adc
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-03-21 15:06:45 -0300

    refactor: findMovieByTitleCustom to order by portuguese title

--- src/main/java/com/kronusboss/cine/adapter/movie/repository/jpa/MovieRepository.java
+++ src/main/java/com/kronusboss/cine/adapter/movie/repository/jpa/MovieRepository.java
@@ -17,7 +17,7 @@ public interface MovieRepository extends JpaRepository<Movie, UUID> {
 	Movie findByTmdbId(Long tmdbId);
 
 	@Query(value = "select * from movie where portuguese_title ILIKE CONCAT('%', :title, '%')"
-			+ " or original_title ILIKE CONCAT('%', :title, '%') or english_title ILIKE CONCAT('%', :title, '%')", nativeQuery = true)
+			+ " or original_title ILIKE CONCAT('%', :title, '%') or english_title ILIKE CONCAT('%', :title, '%') ORDER BY portuguese_title", nativeQuery = true)
 	Page<Movie> findMovieByTitleCustom(@Param("title") String title, Pageable pageable);
 
 	Page<Movie> findAll(Pageable pageable);
