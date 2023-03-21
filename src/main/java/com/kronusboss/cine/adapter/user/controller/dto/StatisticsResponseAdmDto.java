7d206d656026cc5e3dbda7d670f027ce85cc6743
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-03-21 15:07:49 -0300

    feat: added 2 types of Statistics responses

--- src/main/java/com/kronusboss/cine/adapter/user/controller/dto/StatisticsResponseAdmDto.java
+++ src/main/java/com/kronusboss/cine/adapter/user/controller/dto/StatisticsResponseAdmDto.java
@@ -5,14 +5,16 @@ import com.kronusboss.cine.user.domain.Statistics;
 import lombok.Data;
 
 @Data
-public class StatisticsResponseDto {
+public class StatisticsResponseAdmDto {
 
 	private int ratingsGiven;
 	private int registeredMovies;
+	private int consecutiveFailedLoginAttempts;
 
-	public StatisticsResponseDto(Statistics statistics) {
+	public StatisticsResponseAdmDto(Statistics statistics) {
 		ratingsGiven = statistics.getRatingsGiven();
 		registeredMovies = statistics.getRegisteredMovies();
+		consecutiveFailedLoginAttempts = statistics.getConsecutiveFailedLoginAttempts();
 	}
 
 }
