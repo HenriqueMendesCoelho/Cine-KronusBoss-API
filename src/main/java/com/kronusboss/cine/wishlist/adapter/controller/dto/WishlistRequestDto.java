01eb32a55812e3880cbd70b58b0948dd93560e46
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-08-03 21:36:50 -0300

    fix: remove incorrect size validation WishlistRequestDto

--- src/main/java/com/kronusboss/cine/wishlist/adapter/controller/dto/WishlistRequestDto.java
+++ src/main/java/com/kronusboss/cine/wishlist/adapter/controller/dto/WishlistRequestDto.java
@@ -9,7 +9,6 @@ import org.hibernate.validator.constraints.Length;
 import com.kronusboss.cine.wishlist.domain.Wishlist;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,7 +24,6 @@ public class WishlistRequestDto {
 	@Length(min = 3, max = 30)
 	private String name;
 
-	@Size(max = 50)
 	private List<MovieWishlistRequestDto> moviesWishlists;
 	private boolean shareable;
 
