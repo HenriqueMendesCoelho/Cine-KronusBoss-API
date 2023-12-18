c5064f92d75a2d1a584d1920a10949b33c37f1f4
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2023-12-18 16:29:30 -0300

    fix: primary bean MovieJpaRepository

--- src/main/java/com/kronusboss/cine/movie/adapter/repository/jpa/MovieJpaRepository.java
+++ src/main/java/com/kronusboss/cine/movie/adapter/repository/jpa/MovieJpaRepository.java
@@ -2,6 +2,7 @@ package com.kronusboss.cine.movie.adapter.repository.jpa;
 
 import java.util.UUID;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.kronusboss.cine.movie.domain.Movie;
 
+@Primary
 public interface MovieJpaRepository extends JpaRepository<Movie, UUID>, JpaSpecificationExecutor<Movie> {
 
 	@Transactional(readOnly = true)
