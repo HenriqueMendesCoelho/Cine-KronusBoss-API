4e212a6ec00b32ece755b2d0778baaf90765486e
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-03-30 00:43:12 -0300

    refactor: Exclude 'Nota:' text when ShowNotes flag is false EmbedRequestDto Discord

--- src/main/java/com/kronusboss/cine/discord/adapter/repository/rest/dto/EmbedRequestDto.java
+++ src/main/java/com/kronusboss/cine/discord/adapter/repository/rest/dto/EmbedRequestDto.java
@@ -1,5 +1,12 @@
 package com.kronusboss.cine.discord.adapter.repository.rest.dto;
 
+import com.kronusboss.cine.movie.domain.Movie;
+import com.kronusboss.cine.movie.domain.MovieNote;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
@@ -9,14 +16,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
-import com.kronusboss.cine.movie.domain.Movie;
-import com.kronusboss.cine.movie.domain.MovieNote;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -37,20 +36,35 @@ public class EmbedRequestDto {
 	public OffsetDateTime timestamp;
 
 	public EmbedRequestDto(Movie movie) {
-		title = "%s | Nota: %s".formatted(movie.getPortugueseTitle(), getAvgNotes(movie));
+		title = movie.isShowNotes()
+				? "%s | Nota: %s".formatted(movie.getPortugueseTitle(), getAvgNotes(movie))
+				: movie.getPortugueseTitle();
 		image = new Image(movie.getUrlImage());
 		color = BLUE_COLOR_CODE;
 		url = String.format("%s/movie/%s", URL_REDIRECT, movie.getId());
 		footer = new Footer("Cadastrado por: %s".formatted(movie.getUser().getName()));
-		fields = movie.getNotes() != null ? movie.getNotes().stream().map(Field::new).collect(Collectors.toList())
+		fields = movie.getNotes() != null
+				? movie.getNotes().stream().map(Field::new).collect(Collectors.toList())
 				: new ArrayList<>();
 		timestamp = movie.getCreatedAt();
+	}
+
+	private String getAvgNotes(Movie movie) {
+		if (movie.getNotes() == null) {
+			return "0.0";
+		}
+
+		List<Integer> notes = movie.getNotes().stream().map(n -> n.getNote()).collect(Collectors.toList());
+		double avgNote = notes.stream().mapToDouble(d -> d).average().orElse(0.0);
+		DECIMAL_FORMAT.setRoundingMode(RoundingMode.HALF_EVEN);
+
+		return Objects.toString(DECIMAL_FORMAT.format(avgNote), "0.0");
 	}
 
 	@Data
 	@AllArgsConstructor
 	@NoArgsConstructor
-	static class Field {
+	public static class Field {
 		public String name;
 		public String value;
 		public boolean inline;
@@ -65,26 +79,14 @@ public class EmbedRequestDto {
 	@Data
 	@AllArgsConstructor
 	@NoArgsConstructor
-	static class Footer {
+	public static class Footer {
 		public String text;
 	}
 
 	@Data
 	@AllArgsConstructor
 	@NoArgsConstructor
-	static class Image {
+	public static class Image {
 		public String url;
-	}
-
-	private String getAvgNotes(Movie movie) {
-		if (movie.getNotes() == null) {
-			return "0.0";
-		}
-
-		List<Integer> notes = movie.getNotes().stream().map(n -> n.getNote()).collect(Collectors.toList());
-		double avgNote = notes.stream().mapToDouble(d -> d).average().orElse(0.0);
-		DECIMAL_FORMAT.setRoundingMode(RoundingMode.HALF_EVEN);
-
-		return Objects.toString(DECIMAL_FORMAT.format(avgNote), "0.0");
 	}
 }
