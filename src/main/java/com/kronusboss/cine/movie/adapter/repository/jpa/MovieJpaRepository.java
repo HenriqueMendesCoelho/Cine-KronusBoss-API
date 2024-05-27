5884ca02b74b8ab32d9270e736b71d4ae4c135b4
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-05-27 10:00:51 -0300

    refactor: remove unnecessary @Override annotation on MovieJpaRepository

--- src/main/java/com/kronusboss/cine/movie/adapter/repository/jpa/MovieJpaRepository.java
+++ src/main/java/com/kronusboss/cine/movie/adapter/repository/jpa/MovieJpaRepository.java
@@ -1,7 +1,6 @@
 package com.kronusboss.cine.movie.adapter.repository.jpa;
 
-import java.util.UUID;
-
+import com.kronusboss.cine.movie.domain.Movie;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.kronusboss.cine.movie.domain.Movie;
+import java.util.UUID;
 
 @Primary
 public interface MovieJpaRepository extends JpaRepository<Movie, UUID>, JpaSpecificationExecutor<Movie> {
@@ -17,7 +16,6 @@ public interface MovieJpaRepository extends JpaRepository<Movie, UUID>, JpaSpeci
 	@Transactional(readOnly = true)
 	Movie findByTmdbId(Long tmdbId);
 
-	@Override
 	Page<Movie> findAll(Pageable pageable);
 
 }
