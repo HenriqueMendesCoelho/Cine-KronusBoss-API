54430ea6f7daf3bf9ee818c6060c3b6304124707
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-09-20 13:41:26 -0300

    fix: correct unique constraints and remove unused imports in MoviesWishlists

--- src/main/java/com/kronusboss/cine/wishlist/domain/MoviesWishlists.java
+++ src/main/java/com/kronusboss/cine/wishlist/domain/MoviesWishlists.java
@@ -1,32 +1,18 @@
 package com.kronusboss.cine.wishlist.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "movie_wishlist_user_wishlist",
 		uniqueConstraints = { @UniqueConstraint(columnNames = { "wishlist_id", "movie_id" }),
-				@UniqueConstraint(columnNames = { "movie_id", "wishlist_order" }) })
+				@UniqueConstraint(columnNames = { "wishlist_id", "wishlist_order" }) })
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
 public class MoviesWishlists {
-
-	@EmbeddedId
-	private MoviesWishlistsKey id;
 
 	@ManyToOne
 	@MapsId("wishlistId")
@@ -37,6 +23,9 @@ public class MoviesWishlists {
 	@MapsId("movieId")
 	@JoinColumn(name = "movie_id")
 	MovieWishlist movieWishlist;
+
+	@EmbeddedId
+	private MoviesWishlistsKey id;
 
 	@Column
 	private Integer wishlistOrder;
